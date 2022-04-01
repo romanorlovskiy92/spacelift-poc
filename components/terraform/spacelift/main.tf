f038a187a10ded577256f336e27688c1638cf22b
Roman Orlovskiy <roman.orlovskiy@celsius.network>
2022-04-01 19:03:19 +0300

    check if I can use my own fork

--- components/terraform/spacelift/main.tf
+++ components/terraform/spacelift/main.tf
@@ -1,7 +1,7 @@
 provider "spacelift" {}
 
 module "spacelift" {
-  source  = "git@github.com:romanorlovskiy92/terraform-spacelift-cloud-infrastructure-automation.git"
+  source  = "https://github.com/romanorlovskiy92/terraform-spacelift-cloud-infrastructure-automation.git"
   # version = "v0.47.0"
 
   branch             = var.branch
