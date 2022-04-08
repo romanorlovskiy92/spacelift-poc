e93f3bd01bd4a641373ac1ad3a6e83809766934c
Roman Orlovskiy <roman.orlovskiy@celsius.network>
2022-04-08 13:16:30 +0300

    adding 'account' and other components

--- components/terraform/account-map/remote-state.tf
+++ components/terraform/account-map/remote-state.tf
@@ -1,10 +1,10 @@
 module "accounts" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "0.19.0"
+  version = "0.22.1"
 
   component               = "account"
-  privileged              = true
-  stack_config_local_path = "../../../stacks"
+  # privileged              = true
+  # stack_config_local_path = "../../../stacks"
 
   context = module.this.context
 }
